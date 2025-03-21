d64f9284b3130c0d6310577183a3be85ff64eeb7
varshney-23 <ayushivarshney40@gmail.com>
2025-03-21 16:48:34 +0530

    calc wages for a month

--- Employee.js
+++ Employee.js
@@ -39,3 +39,17 @@ switch(workingHours){
 let wage = wagePerHour*workingHours;
 
 console.log("Daily wage generated for employee is ",wage);
+
+//UC-3
+const numberOfWorkingDays = 20;
+
+let empHrs = 0;
+
+for(let i = 1; i<=numberOfWorkingDays;i++){
+    let empCheck = Math.floor(Math.random()*10)%3;    //getting fulltime, partime or notime status
+    empHrs += getWorkingHours(empCheck);
+}
+
+let empWage = empHrs*wagePerHour;
+console.log("Monthly Wage of the Employee for hours "+empHrs+" is "+empWage);
+

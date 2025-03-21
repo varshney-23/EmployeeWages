394622f3130fc6427baa0e3cc4e8ff8bb2cbc42f
varshney-23 <ayushivarshney40@gmail.com>
2025-03-21 16:31:33 +0530

    UC1

---
+++ Employee.js
@@ -0,0 +1,11 @@
+//checking 
+const present = 1;
+
+let isPresent = Math.floor(Math.random()*10)%2;
+
+if(isPresent == present){
+    console.log("Employee is Present");
+}
+else{
+    console.log("Employee is not Present");
+}
